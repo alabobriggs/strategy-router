5d65dbd61a1b84d811eebbab649a76e6d8f143af
alabobriggs <alabobriggs@gmail.com>
2022-09-29 12:42:03 +0100

    update poolID

--- contracts/strategies/Stargate/StargateUsdt.sol
+++ contracts/strategies/Stargate/StargateUsdt.sol
@@ -9,7 +9,7 @@ contract StargateUsdt is StargateBase {
     constructor(StrategyRouter _strategyRouter)
         StargateBase(
             _strategyRouter,
-            2, // poolId
+            0, // poolId
             ERC20(0x55d398326f99059fF775485246999027B3197955), // tokenA - USDT 
             ERC20(0x9aA83081AA06AF7208Dcc7A4cB72C94d057D2cda) // lpToken USDT
         )
