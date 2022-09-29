1f7712b5e72c3feb129b3f5b34f2462908e6ddaf
alabobriggs <alabobriggs@gmail.com>
2022-09-29 15:49:42 +0100

    add stargate testing

--- test/strategies.withdrawal.spec.js
+++ test/strategies.withdrawal.spec.js
@@ -3,6 +3,7 @@ const strategyTest = require("./shared/strategyTest");
 describe("Test strategies", function () {
 
   let strategies = [
+    { name: "StargateUsdt" },
     { name: "BiswapUsdcUsdt" },
     { name: "BiswapBusdUsdt" },
   ];
